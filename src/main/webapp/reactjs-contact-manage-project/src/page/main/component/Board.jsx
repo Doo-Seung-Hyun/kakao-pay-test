233864513050389821bf6d696525a0df5e9b5281
Doo-Seung-Hyun <doosh17@naver.com>
2022-06-03 06:43:41 +0900

    init msg

--- src/main/webapp/reactjs-contact-manage-project/src/page/main/component/Board.jsx
+++ src/main/webapp/reactjs-contact-manage-project/src/page/main/component/Board.jsx
@@ -69,26 +69,6 @@ function refreshData({setInquiriesData, setErrorHttpStatus}){
 
 
 function Board({setInquiryId}) {
-    const getInquiriesList=()=>{
-        let inquiriesList = [
-            {inquiryId:25, title: '안녕하세요', customerId: 'qqwer434', createdDateTime: '2022.05.23 18:23:55', userId: null,	userNm	:'', acceptedDateTime: null, finishedDateTime:null, isFinished: null	},
-            {inquiryId:24, title: '문의드려요', customerId: 'doosh17', createdDateTime: '2022.05.22 17:23:55', userId: null, 	userNm	:'', acceptedDateTime: null, finishedDateTime:null, isFinished: null	},
-            {inquiryId:23, title: '도대체 언제되나요', customerId: 'zhzhektm23', createdDateTime: '2022.05.19 18:23:56', userId: null,	userNm:'', acceptedDateTime: null, finishedDateTime:null, isFinished: null	},
-            {inquiryId:22, title: '전화 통화가 너무힘들어요', customerId: 'sudent5353', createdDateTime: '2022.05.18 17:23:56', userId: 4,	userNm:'배수나', acceptedDateTime: '2022.05.23 18:23:55', finishedDateTime:null,		isFinished: 'N'},
-            {inquiryId:21, title: '아 짜증나네요', customerId: 'kimkimko', createdDateTime: '2022.05.15 18:23:57', userId: 3,	userNm: '두승현', acceptedDateTime: '2022.05.22 17:23:55', finishedDateTime:null,		isFinished: 'N'},
-            {inquiryId:20, title: '감사합니다', customerId: 'kakaosarang', createdDateTime: '2022.05.15 17:23:57', userId: 4,	userNm: '배수나', acceptedDateTime: '2022.05.19 18:23:56', finishedDateTime:null,		isFinished: 'N'},
-            {inquiryId:19, title: '너무 고맙습니다', customerId: 'qwepo3', createdDateTime: '2022.05.14 18:23:58', userId: 1,	userNm: '김카카오', acceptedDateTime: '2022.05.18 17:23:56', finishedDateTime: '2022.05.23 18:23:55',	isFinished: 'N'},
-            {inquiryId:18, title: '만족스럽네요', customerId: 'ccvd443dd', createdDateTime: '2022.05.14 17:23:58', userId: 2,	userNm:	'이페이', acceptedDateTime: '2022.05.15 18:23:57',	finishedDateTime: '2022.05.22 17:23:55',	isFinished: 'Y'},
-            {inquiryId:17, title: '테스트입니다', customerId: 'asdf!sd%%', createdDateTime: '2022.05.13 18:23:59', userId: 3,	userNm: '두승현', acceptedDateTime: '2022.05.15 17:23:57', finishedDateTime:	'2022.05.19 18:23:56',	isFinished: 'Y'},
-            {inquiryId:16, title: '상담사를 칭찬합니다', customerId: 'znznektm86', createdDateTime: '2022.05.13 17:23:59', userId: 4,	userNm: '배수나', acceptedDateTime: '2022.05.14 18:23:58', finishedDateTime: '2022.05.18 17:23:56', 	isFinished: 'Y'}
-            // {inquiryId:15, title: '제 물건을 찾습니다', customerId: 'qojdioj%%', createdDateTime: '2022.05.13 14:23:22', userId: 1,	userNm: '김카카오', acceptedDateTime: '2022.05.14 17:23:58',	finishedDateTime: '2022.05.15 18:23:57',	isFinished: 'Y'},
-            // {inquiryId:14, title: '출발안하나요?', customerId: 'pporrorro33', createdDateTime: '2022.05.12 17:23:13', userId: 2,	userNm	: '이페이', acceptedDateTime: '2022.05.13 18:23:59', finishedDateTime: '2022.05.15 17:23:57', isFinished: 'Y'},
-            // {inquiryId:13, title: '안녕하세요', customerId: 'salesmantop', createdDateTime: '2022.05.12 12:55:44', userId: 1,	userNm: '김카카오', acceptedDateTime: '2022.05.13 17:23:59', finishedDateTime: '2022.05.14 18:23:58', isFinished: 'Y'},
-            // {inquiryId:12, title: '문의드려요', customerId: 'topsecret1!', createdDateTime: '2022.05.12 10:23:14', userId: 3,	userNm: '두승현', acceptedDateTime: '2022.05.13 14:23:22	', finishedDateTime: '2022.05.14 17:23:58',	isFinished: 'Y'},
-            // {inquiryId:11, title: '도대체 언제되나요', customerId: 'overtherainbow', createdDateTime: '2022.05.12 09:23:33', userId: 4,	userNm: '배수나', acceptedDateTime: '2022.05.12 17:23:13	', finishedDateTime: '2022.05.13 14:23:22',	isFinished: 'Y'}
-        ];
-        return inquiriesList;
-    };
 
     const [inquiriesData, setInquiriesData] = useState(null);
     const [isAutomatic, setAutomatic] = useState(true);
@@ -150,7 +130,7 @@ function Board({setInquiryId}) {
                 <button className={"upd-btn btn"} onClick={()=>{doAcceptCheckedList(states,setStates)}}>접수</button>
             </div>
             <Table data={inquiriesData==null? null:[...inquiriesData.outList]} setInquiryId={setInquiryId} />
-            <Pagination pageNo={13}></Pagination>
+            <Pagination pageNo={1}></Pagination>
         </>
     )
 }
